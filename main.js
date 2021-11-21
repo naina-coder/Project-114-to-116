5f77943c4d9179f84f6cc2a528cc9ff27162550c
naina-coder <82169500+naina-coder@users.noreply.github.com>
2021-11-21 14:53:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(350, 350);
+    canvas.center();
+
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('You_Have_a_Giant_Moustache.png');
+}
